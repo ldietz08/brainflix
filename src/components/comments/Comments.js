8777bb5da85aa3e228ee954619136ab1e14a0b41
Lakpa Dietz <ldietz086@gmail.com>
2022-11-06 16:38:06 -0800

    Assigned unique key to video list and comments and adjusted desktop styling

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -4,40 +4,28 @@ import "./Comments.scss";
 const Comments = ({ comments }) => {
   return (
     <>
-    {/* Ternary operator - If array contains comments render them, otherwise render alternative JSX */}
+      {/* Ternary operator - If array contains comments render them, otherwise render alternative JSX */}
       {comments.length > 0 ? (
         <div className="comments__content-container">
           {comments.map((comment) => (
-            <>
-              <div className="comments__container-main">
-                <div className="comments__content-logo"></div>
-                <div className="comments__item">
-                  <div className="comments__content-wrapper">
-                    <div className="comments__content">
-                      <h3 className="comments__content-name" key={comment.name}>
-                        {comment.name}
-                      </h3>
-                      <p
-                        className="comments__content-date"
-                        key={comment.timestamp}
-                      >
-                        {new Date(comment.timestamp).toLocaleDateString(
-                          "en-US",
-                          {
-                            year: "numeric",
-                            month: "2-digit",
-                            day: "2-digit",
-                          }
-                        )}
-                      </p>
-                    </div>
-                    <p className="comments__content-text" key={comment.comment}>
-                      {comment.comment}
+            <div className="comments__container-main" key={comment.id}>
+              <div className="comments__content-logo"></div>
+              <div className="comments__item">
+                <div className="comments__content-wrapper">
+                  <div className="comments__content">
+                    <h3 className="comments__content-name">{comment.name}</h3>
+                    <p className="comments__content-date">
+                      {new Date(comment.timestamp).toLocaleDateString("en-US", {
+                        year: "numeric",
+                        month: "2-digit",
+                        day: "2-digit",
+                      })}
                     </p>
                   </div>
+                  <p className="comments__content-text">{comment.comment}</p>
                 </div>
               </div>
-            </>
+            </div>
           ))}
         </div>
       ) : (
--- src/components/videoList/VideoList.js
+++ src/components/videoList/VideoList.js
@@ -3,14 +3,13 @@ import "./VideoList.scss";
 
 const VideoList = ({ videos, onClick }) => {
   return (
-    <aside className="videoList__container">
-      <h4 className="videoList__title-main">Next Videos</h4>
-      {videos.map((video) => (
-        <>
-          <div className="videoList__items-wrapper">
+    <>
+      <aside className="videoList__container">
+        <h4 className="videoList__title-main">Next Videos</h4>
+        {videos.map((video) => (
+          <div className="videoList__items-wrapper" key={video.id}>
             <div className="videoList__item">
               <img
-                key={video.id}
                 src={video.image}
                 alt={video.title}
                 className="videoList__thumbnail"
@@ -22,9 +21,9 @@ const VideoList = ({ videos, onClick }) => {
               <p className="videoList__creator">{video.channel}</p>
             </div>
           </div>
-        </>
-      ))}
-    </aside>
+        ))}
+      </aside>
+    </>
   );
 };
 
